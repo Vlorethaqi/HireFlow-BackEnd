d61e2f2f034977d318b71bbdb88866e3f66c66ae
PranveraGashi <121582082+Pranvera21@users.noreply.github.com>
2026-05-11 22:24:26 +0200

    import added to index

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,10 @@
 import User from "./User.js";
 import Company from "./Company.js";
+import Job from "./Job.js";
+import Department from "./Department.js";
+import JobRequirement from "./JobRequirement.js";
+import Skill from "./Skill.js";
+import JobSkill from "./JobSkill.js";
 
 Company.hasMany(User, {
     foreignKey: "companyId",
@@ -13,3 +18,11 @@ export {
     User,
     Company,
 };
+Department.hasMany(Job);
+Job.belongsTo(Department);
+
+Job.hasMany(JobRequirement);
+JobRequirement.belongsTo(Job);
+
+Job.belongsToMany(Skill, { through: JobSkill });
+Skill.belongsToMany(Job, { through: JobSkill });
