200f5583b2f4a77f4db7e1dd236c10f7751c22ff
PranveraGashi <121582082+Pranvera21@users.noreply.github.com>
2026-05-11 22:19:35 +0200

    Job model created

---
+++ src/models/Job.js
@@ -0,0 +1,44 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const Job = sequelize.define("Job", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+
+  location: {
+    type: DataTypes.STRING
+  },
+
+  salary: {
+    type: DataTypes.FLOAT
+  },
+
+  status: {
+    type: DataTypes.ENUM("OPEN", "CLOSED"),
+    defaultValue: "OPEN"
+  },
+
+  companyId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+
+  departmentId: {
+    type: DataTypes.INTEGER
+  }
+});
+
+export default Job;
