460f4bb59915a8bbfac4dd5943ba25722f5a958c
rinesa-bejic <rinesa.bejic@student.uni-pr.edu>
2026-05-12 21:01:02 +0200

    CandidateProfile model created

---
+++ src/models/CandidateProfile.js
@@ -0,0 +1,51 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const CandidateProfile = sequelize.define("CandidateProfile", {
+    bio: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    location: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+
+    experienceYears: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+
+    education: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+
+    cvUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+
+    linkedinUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+
+    githubUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+});
+
+export default CandidateProfile;
