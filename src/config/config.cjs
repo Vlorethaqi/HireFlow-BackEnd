9a3e8ca8788193e8f404b8b743c037e5a2936f85
PranveraGashi <121582082+Pranvera21@users.noreply.github.com>
2026-05-22 14:53:42 +0200

    config.cjs

--- src/config/config.cjs
+++ src/config/config.cjs
@@ -1,28 +1,9 @@
-require("dotenv").config();
-
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-  },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-  },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-  },
-};
+    development: {
+      username: "postgres",
+      password: "pranvera2110",
+      database: "hireflow_db",
+      host: "127.0.0.1",
+      dialect: "postgres"
+    }
+  };
