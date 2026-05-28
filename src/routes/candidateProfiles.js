008dce1527b0edc279c29146922f37f72a8bc12a
rinesa-bejic <rinesa.bejic@student.uni-pr.edu>
2026-05-28 21:35:38 +0200

    added changes to candidateProfiles

--- src/routes/candidateProfiles.js
+++ src/routes/candidateProfiles.js
@@ -1,15 +1,20 @@
 import express from "express";
 import {
     getAllProfiles,
+    getMyProfile,
+    upsertMyProfile,
     getProfileById,
     createProfile,
     updateProfile,
     deleteProfile
 } from "../controllers/candidateProfileController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getAllProfiles);
+router.get("/me", authMiddleware, getMyProfile);
+router.put("/me", authMiddleware, upsertMyProfile);
 router.get("/:id", getProfileById);
 router.post("/", createProfile);
 router.put("/:id", updateProfile);
